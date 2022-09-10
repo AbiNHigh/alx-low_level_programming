3085bd7439c361ea7f6be230d6cd466072d19454
Your Name <you@example.com>
2022-09-10 07:01:37 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,30 @@
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+
+*main - print the string in the put function                                                                                            
+
+*                                                                                                                                       
+
+*Description: using the main function                                                                                                   
+
+*this programs prints "programming is like building a multilingual puzzle                                                               
+
+*Return 0                                                                                                                               
+
+*/                                                                                                                                      
+
+intt main(void)                                                                                                                         
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+        puts("\"programming is like building a multilingual puzzle");                                                                   
+
+        return (0); 
+
+} 
+
