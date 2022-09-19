91f85ade85d7f6256302f970b601ebb064d4220f
Your Name <you@example.com>
2022-09-19 15:15:56 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 _ resets the value of given integer to 98
- * @n: First operand
- *
+ * reset_to_98 - resets the value of given integer to 98
+ * @n: parameter
+ * Return : nothing
  */
 
 void reset_to_98(int *n)
